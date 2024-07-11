88d29cc41f22c9e8f5748b5f9b64fc3a77a15fe3
Francho <f.perez475@gmail.com>
2024-07-12 00:43:27 +0200

    Delete Student model

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
-from models.student import Student
 from models.team.character import Character
 from models.team.perk import Perk
 from models.team.team import Team
 from models.user.admin import Admin
 
-__all__ = [Student, Admin, Perk, Team, Character]
+__all__ = [Admin, Perk, Team, Character]
